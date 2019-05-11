792ee7d3171258dbba1ff593e8ad0d05e5ce03ed
webguy83 <curtisyacboski@gmail.com>
2019-05-10 19:39:45 -0700

    added ingredients to the pizza container

--- src/components/Pizza/Pizza.js
+++ src/components/Pizza/Pizza.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import PizzaIngredient from './PizzaIngredient/PizzaIngredient';
 
+import pizzaGenericStyles from './PizzaIngredient/PizzaIngredient.module.css';
+
 const Pizza = () => {
     return (
-       <PizzaIngredient />
+        <div className={pizzaGenericStyles.plate}>
+            <div className={pizzaGenericStyles.pizzaBase1}>
+                <div className={pizzaGenericStyles.pizzaBase2}>
+                    <PizzaIngredient type="pepperoni" />
+                    <PizzaIngredient type="veggie" />
+                    <PizzaIngredient type="pineapple" />
+                    <PizzaIngredient type="mushroom" />
+                    <PizzaIngredient type="olives" />
+                </div>
+            </div>
+            <div className={pizzaGenericStyles.handle}>
+                <div className={pizzaGenericStyles.handleHole}></div>
+            </div>
+        </div>
     );
 };
 

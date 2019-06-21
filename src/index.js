1f4c7839e41b5c0a6fe5e6c653fc272e763f5559
Curtis Yacboski <curtisyacboski@gmail.com>
2019-06-20 23:48:56 -0700

    added redux thunk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import pizzaMakerReducers from './store/reducers/pizzaMaker';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(pizzaMakerReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(pizzaMakerReducers, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
 
